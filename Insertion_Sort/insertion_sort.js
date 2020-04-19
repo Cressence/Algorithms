0aa5a3026cf682cfedbb252d0d762e7c624fa6d9
Cressence <cressencey@gmail.com>
2020-04-19 19:20:49 +0100

    Complete insertion sort algoritm

--- Insertion_Sort/insertion_sort.js
+++ Insertion_Sort/insertion_sort.js
@@ -1,3 +1,21 @@
 function insertion_sort(unsorted_array) {
+    let position;
+    let valueToInsert;
+    let array_length= unsorted_array.length;
 
+    for (let i = 1; i < array_length; i++) {
+        valueToInsert = unsorted_array[i];
+        position = i;
+
+        while((position > 0) && (unsorted_array[position - 1] > valueToInsert)) {
+            unsorted_array[position] = unsorted_array[position - 1];
+            position = position - 1;
+        }
+
+        unsorted_array[position] = valueToInsert;
+    }
+    return unsorted_array;
 }
+
+const test = insertion_sort([7,34,7,2,45,0,2,2,7,8,28,11,34]);
+console.log(test);
